494399d5ac6b742dbbfea4474b8861e7a33c8a0d
Giordano Bruno <giordanobrunotj@hotmail.com>
2023-08-01 16:23:26 +0100

    trying to make responsive

---
+++ script.js
@@ -0,0 +1,18 @@
+function showContent1() {
+  document.getElementById("content").innerHTML = "hello 1";
+}
+function showContent2() {
+  document.getElementById("content").innerHTML = "hello 2";
+}
+function showContent3() {
+  document.getElementById("content").innerHTML = "hello 3";
+}
+function showContent4() {
+  document.getElementById("content").innerHTML = "hello 4";
+}
+function showContent5() {
+  document.getElementById("content").innerHTML = "hello 5";
+}
+function showContent6() {
+  document.getElementById("content").innerHTML = "hello 6";
+}
