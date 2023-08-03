49c8d0661ee65e75f821a7e002d90fe628648063
Giordano Bruno <giordanobrunotj@hotmail.com>
2023-08-03 16:48:11 +0100

    added content button

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function showContent1() {
   document.getElementById("content").innerHTML = "hello 1";
+
+
 } 
 function showContent2() {
   document.getElementById("content").innerHTML = "hello 2"; 
@@ -8,7 +10,9 @@ function showContent3() {
   document.getElementById("content").innerHTML = "hello 3";
 }
 function showContent4() {
-  document.getElementById("content").innerHTML = "hello 4";
+  document.getElementById(
+    "content"
+  ).innerHTML = `<div id="youtubeContent"><iframe src="https://www.youtube.com/embed/nkpma_EiUlk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></div>`;
 }
 function showContent5() {
   document.getElementById("content").innerHTML = "hello 5";
