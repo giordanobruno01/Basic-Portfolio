22e46ec1f169050f731f7929584a36567746b68b
Giordano Bruno <giordanobrunotj@hotmail.com>
2023-08-06 13:43:34 +0100

    widget added

--- script.js
+++ script.js
@@ -3,78 +3,101 @@ function showContent1() {
     "content"
   ).innerHTML = `<div id= "contentCv">About me</div>`;
 }
-function showContent2() { 
-  document.getElementById(
-    "content"
-  ).innerHTML = `<div id= "contentCv">
-<p><h6>KEY SKILLS</h6> 
-•	Applications: Microsoft Office Suite (Word, Excel, PowerPoint, Outlook) 
-•	Programming Languages: Java, CSS, HTML5, JavaScript, Python, Spring Boot, API
-•	Database Systems: MySQL, SQL Server 
-•	Operating systems and Network 
-•	Git and GitHub
-•	Customer Relationship Management Systems
-•	Customer Service Experience: Hairdresser
-•	Administration Skills 
-•	Driving Licence 
-•	Languages: English, Portuguese, Spanish
-•	KYC: Know Your Customer 
+function showContent2() {
+  document.getElementById("content").innerHTML = `<div id= "contentCv">
+<h4>KEY SKILLS</h4> 
+<ul style="list-style-type:"none">
+<li>Applications: Microsoft Office Suite (Word, Excel, PowerPoint, Outlook)</li> 
+<li>Programming Languages: Java, CSS, HTML5, JavaScript, Python, Spring Boot, API</li>
+<li>Database Systems: MySQL, SQL Server</li> 
+<li>Operating systems and Network </li>
+<li>Git and GitHub</li>
+<li>Customer Service Experience: Hairdresser</li>
+<li>Administration Skills</li> 
+<li>Driving Licence </li>
+<li>Languages: English, Portuguese, Spanish</li>
+<li>KYC: Know Your Customer</li> </ul>
 </p>
 <p>
-<h6>EDUCATION AND TRAINING</h6>  
-2022 - 2023	-		National College of Ireland, Dublin
-				Higher Diploma in Science in Computing
-				NFQ Level 8
-
-2014 - 2017	-		Unopar - Universidade Norte do Parana, Brazil
-				Bachelor’s degree in Social Work
-				NFQ Level 8
-
-2009 – 2011	-		Colegio Municipal Boa Vista do Tupim, Brazil
-Leaving Certificate
-</p>
+<h4>EDUCATION AND TRAINING</h4>  
 <p>
-<h6>WORK EXPERIENCE</h6>
-2023	           	Delta Capita
-				KYC Analyst
+  <strong>2022 - 2023	-		National College of Ireland, Dublin - </strong>
+          Higher Diploma in Science in Computing
+          NFQ Level 8
+</p>
 
-•	Compliance with regulations: Ensuring adherence to local and international regulatory requirements, such as Anti-Money Laundering (AML) and Counter Financing of Terrorism (CFT) laws.
-•	Documentation and record-keeping: Maintaining accurate and up-to-date customer records, including documentation of verification processes and ongoing monitoring.
-•	Monitoring and reporting suspicious activities: Identifying potential fraudulent or suspicious transactions.
-•	Verifying the identity and background of customers using various tools and documents, such as ID cards, passports, and utility bills.
-2023	           	Testlio 
-				Manual Tester/Freelancer 
-•	Follow test cases and test scenarios to execute various tests on the software
-•	Meticulously identify and document defects, inconsistencies, and usability issues in the software. Provide detailed bug reports to developers to facilitate efficient debugging and resolution.
-•	Assess the software's user experience, ensuring it meets usability standards and provides a smooth, intuitive interface for end-users.
-•	Validate that the software meets the specified requirements and verify that new features or changes do not adversely affect existing functionalities.
-•	Maintain comprehensive testing documentation, including test reports, test results, and test logs, to track progress and communicate findings effectively.
-2022 - 2023		Accenture, Dublin
-(March 2023 – May 2023)	Data Manager
-•	Reviewing documents from customers migrating to another bank complying with KYC (Know Your Customer) standard reducing their risk exposure by verifying the identities
-•	Work directly with an Irish financial client
-•	Ability to process data in a fast paced environment
-•	Working as part of a team in a hybrid working environment
-(June 2022 – Feb 2023)      Data Entry Associate
-•	Process accurate indexing documents in the system. Analyse, identity, escalate or correct issues with files.
-•	Work directly with an Irish financial client
-•	Handling customer documents working across multiple products and processes, such as mortgage, finance and leasing and tax returns
-•	Deliver exceptional customer service by ensuring all our customer and legal documents are scanned, routed and stored in the most efficient and effective manner 
-</p></div>
+<p>
+  <strong>2014 - 2017	-		Unopar - Universidade Norte do Parana, Brazil - </strong>
+          Bachelor’s degree in Social Work
+          NFQ Level 8
+</p>
+
+<p>
+<h4>WORK EXPERIENCE</h4>
+           	<ul style="list-style-type:"disc">
+              <p>
+                <strong>
+                  Delta Capita
+                          KYC Analyst - 2023
+                </strong>
+              </p>
+
+<li>• Compliance with regulations: Ensuring adherence to local and international regulatory requirements, such as Anti-Money Laundering (AML) and Counter Financing of Terrorism (CFT) laws.</li>
+<li>• Documentation and record-keeping: Maintaining accurate and up-to-date customer records, including documentation of verification processes and ongoing monitoring.</li>
+<li>• Monitoring and reporting suspicious activities: Identifying potential fraudulent or suspicious transactions.</li>
+<li>• Verifying the identity and background of customers using various tools and documents, such as ID cards, passports, and utility bills.</li></ul>	
+	           	<ul>
+                <p>
+                                <strong>
+                                  Testlio
+                                          Manual Tester/Freelancer 2023
+                                </strong>
+                              </p>
+                <li>• Follow test cases and test scenarios to execute various tests on the software</li>
+                <li>•	Meticulously identify and document defects, inconsistencies, and usability issues in the software. Provide detailed bug reports to developers to facilitate efficient debugging and resolution.</li>
+                <li>•	Assess the software's user experience, ensuring it meets usability standards and provides a smooth, intuitive interface for end-users.</li>
+                <li>•	Validate that the software meets the specified requirements and verify that new features or changes do not adversely affect existing functionalities.</li>
+                <li>•	Maintain comprehensive testing documentation, including test reports, test results, and test logs, to track progress and communicate findings effectively.</li>
+              </ul>
+		<ul>
+      <p><strong>Accenture, Dublin 2022 - 2023</strong></p>
+      <h6>(March 2023 – May 2023)	Data Manager</h6>
+      <li>•	Reviewing documents from customers migrating to another bank complying with KYC (Know Your Customer) standard reducing their risk exposure by verifying the identities</li>
+      <li>•	Work directly with an Irish financial client</li>
+      <li>•	Ability to process data in a fast paced environment</li>
+      <li>•	Working as part of a team in a hybrid working environment</li>
+      <h6>(June 2022 – Feb 2023)      Data Entry Associate</h6>
+      <li>•	Process accurate indexing documents in the system. Analyse, identity, escalate or correct issues with files.</li>
+      <li>•	Work directly with an Irish financial client</li>
+      <li>•	Handling customer documents working across multiple products and processes, such as mortgage, finance and leasing and tax returns</li>
+      <li>
+        •	Deliver exceptional customer service by ensuring all our customer and
+             legal documents are scanned, routed and stored in the most efficient and effective manner
+      </li> </ul>
+      
+        <h4>VOLUNTEER WORK</h4>
+        <ul>
+        <p><strong>AIESEC, Portuguese and English Teacher Feb 2018 - March 2018</strong></p>
+<li>● Creating interactive lessons to help low income kids and teenagers students engage in English and Portuguese class in Colombia</li>
+      </ul>
+</div>
 `;
 }
 function showContent3() {
   document.getElementById(
     "content"
-  ).innerHTML = `<div id= "contentCv">199 South Circular Road, Dublin 8.
-<a href="">giordanobrunotj@hotmail.com </a>
-		Mobile Number: +3530830893275
+  ).innerHTML = `<div id= "contentCv" <address>199 South Circular Road, Dublin 8</address>
+  <a href="mailto:giordanobrunotj@hotmail.com" target="_blank">giordanobrunotj@hotmail.com</a>
+<a href="tel:+3530830893275">+3530830893275</a>
 </div>`;
 }
 function showContent4() {
   document.getElementById(
     "content"
-  ).innerHTML = `<div id="youtubeContent"><iframe src="https://www.youtube.com/embed/nkpma_EiUlk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></div>`;
+  ).innerHTML = `<div id="instagramContent"><iframe id="instagramFrame" src='https://widgets.sociablekit.com/youtube-channel-videos/iframe/176585' ></iframe></div>`;
+  // document.getElementById(
+  //   "content"
+  // ).innerHTML = `<div id="youtubeContent"><iframe src="https://www.youtube.com/embed/nkpma_EiUlk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></div>`;
 }
 function showContent5() {
   document.getElementById(
